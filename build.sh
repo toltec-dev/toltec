6abaaa5b2e670a5184d2b3d07cf1d3f0c217bbb9
Mattéo Delabre <spam@delab.re>
2020-08-29 12:20:42 +0200

    Fix archive creation

--- build.sh
+++ build.sh
@@ -1,20 +1,35 @@
-if [[ $# < 2 ]]; then
-    echo "Missing arguments"
+error() {
+    echo "$1" >&2
     exit 1
-fi
+}
+
+rtar() {
+    tar --sort=name \
+        --owner=0 --group=0 --numeric-owner \
+        --mtime="1970-01-01T00:00Z" \
+        --pax-option=exthdr.name=%d/PaxHeaders/%f,delete=atime,delete=ctime \
+        $@
+}
+
+[[ $# < 2 ]] && error "Missing arguments"
 
 package="$1"
 workdir="$2"
 
-if [ -d "$workdir" ]; then
-    echo "Working directory already exists!"
-    exit 1
-fi
+# Create working directories
+[[ -d $workdir ]] && error "Working directory already exists!"
 
 srcdir="$workdir"/src
 mkdir -p "$srcdir"
+
 pkgdir="$workdir"/pkg
 mkdir -p "$pkgdir"
+
+ctldir="$workdir"/control
+mkdir -p "$ctldir"
+
+ardir="$workdir"/ar
+mkdir -p "$ardir"
 
 # Load reMarkable toolchain
 source /usr/local/oecore-x86_64/environment-setup-cortexa9hf-neon-oe-linux-gnueabi
@@ -22,34 +37,46 @@ source /usr/local/oecore-x86_64/environment-setup-cortexa9hf-neon-oe-linux-gnuea
 # Load package info and script
 source "$(realpath "$package")"
 
+[[ -z $pkgname ]] && error "Missing or empty “pkgname” variable"
+[[ -z $pkgver ]] && error "Missing or empty “pkgver” variable"
+[[ -z $license ]] && error "Missing or empty “license” variable"
+[[ -z $section ]] && error "Missing or empty “section” variable"
+[[ -z $maintainer ]] && error "Missing or empty “maintainer” variable"
+[[ -z $arch ]] && arch=armv7-3.2
+[[ -z $pkgdesc ]] && error "Missing or empty “description” variable"
+
 # Fetch source files
 git clone --quiet --recursive "$origin" "$srcdir"
-pushd "$srcdir"
-git checkout --quiet "$revision"
-popd
 
 # Run build script
+pushd "$srcdir" > /dev/null
+git checkout --quiet "$revision"
 build
+popd > /dev/null
 
 # Run packaging script
 package
 
-# Create control file
-ctldir="$pkgdir"/CONTROL
+# Create archives
+pkgar="$ardir"/data.tar.gz
+ctlar="$ardir"/control.tar.gz
+versionar="$ardir"/debian-binary
+arar="$workdir/${pkgname}_${pkgver}_${arch}.ipk"
+
+echo "2.0" >> "$versionar"
+rtar --transform="s/^${pkgdir//\//\\\/}/./" -zcf "$pkgar" "$pkgdir"
+
+# Create control file and archive
 ctlfile="$ctldir"/control
 
-mkdir -p "$ctldir"
 echo "Package: $pkgname" >> "$ctlfile"
 echo "Version: $pkgver" >> "$ctlfile"
-echo "Section: $pkgsect" >> "$ctlfile"
+[[ ! -z $pkgdeps ]] && echo "Depends: $pkgdeps" >> "$ctlfile"
+echo "License: $license" >> "$ctlfile"
+echo "Section: $section" >> "$ctlfile"
+echo "Maintainer: $maintainer" >> "$ctlfile"
+echo "Architecture: $arch" >> "$ctlfile"
 echo "Description: $pkgdesc" >> "$ctlfile"
-echo "Maintainer: $pkgmaint" >> "$ctlfile"
-echo "Architecture: armv7-3.2" >> "$ctlfile"
-echo "Priority: optional" >> "$ctlfile"
 
-if [ ! -z "$pkgdeps" ]; then
-    echo "Depends: $pkgdeps" >> "$ctlfile"
-fi
-
-# Create package
-opkg-build "$pkgdir" "$workdir"
+rtar --transform="s/^${ctldir//\//\\\/}/./" -zcf "$ctlar" "$ctldir"
+rtar --transform="s/^${ardir//\//\\\/}/./" -zcf "$arar" "$ardir"
