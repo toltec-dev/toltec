d6dab23c9b5cfde9ef6f944522f41d5086a3bb9b
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2021-01-04 15:47:12 -0700

    Update rm2-suspend-fix.sh

--- package/rm2-suspend-fix/rm2-suspend-fix.sh
+++ package/rm2-suspend-fix/rm2-suspend-fix.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Inspired by https://blog.christophersmart.com/2016/05/11/running-scripts-before-and-after-suspend-with-systemd/comment-page-1/
 
 if [[ "${1}" == "pre" ]]; then
