d069d2269bf07b2db5d0aaad77df2d271df2bfb2
Nathaniel van Diepen <Eeems@users.noreply.github.com>
2021-01-04 15:46:21 -0700

    Update rm2-suspend-fix.sh

--- package/rm2-suspend-fix/rm2-suspend-fix.sh
+++ package/rm2-suspend-fix/rm2-suspend-fix.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-if [ "${1}" == "pre" ]; then
+# Inspired by https://blog.christophersmart.com/2016/05/11/running-scripts-before-and-after-suspend-with-systemd/comment-page-1/
+
+if [[ "${1}" == "pre" ]]; then
     rmmod brcmfmac
-elif [ "${1}" == "post" ]; then
+elif [[ "${1}" == "post" ]]; then
     modprobe brcmfmac
 fi
