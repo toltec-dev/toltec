5f2770516654efb5d7f006da1750b32b78db7f11
Nathaniel van Diepen <nathaniel.vandiepen@gmail.com>
2025-10-29 20:46:50 -0600

    Consolidate

--- scripts/package_build.py
+++ scripts/package_build.py
@@ -12,10 +12,9 @@ from typing import (
     List,
     Optional,
 )
-from shutil import disk_usage, rmtree
-from repo_build import sizeof_fmt
+from shutil import rmtree
+from repo_build import print_disk_usage, cleanup, argparse_add_cleanup
 from build import paths
-from build.repo import Repo
 from toltec import parse_recipe  # type: ignore
 from toltec.builder import Builder  # type: ignore
 from toltec.recipe import Package  # type: ignore
@@ -24,80 +23,70 @@ from toltec.util import argparse_add_verbose, LOGGING_FORMAT  # type: ignore
 
 logger = logging.getLogger(__name__)
 
-parser = argparse.ArgumentParser(description=__doc__)
 
-_ = parser.add_argument(
-    "recipe_name",
-    metavar="RECIPENAME",
-    help="name of the recipe to build",
-)
+def main() -> None:
+    """Build a package"""
+    parser = argparse.ArgumentParser(description=__doc__)
 
-_ = parser.add_argument(
-    "-a",
-    "--arch-name",
-    metavar="ARCHNAME",
-    action="append",
-    help="""only build for the given architecture (can
-    be repeated)""",
-)
+    _ = parser.add_argument(
+        "recipe_name",
+        metavar="RECIPENAME",
+        help="name of the recipe to build",
+    )
 
-_ = parser.add_argument(
-    "packages_names",
-    nargs="*",
-    metavar="PACKAGENAME",
-    help="list of packages to build (default: all packages from the recipe)",
-)
+    _ = parser.add_argument(
+        "-a",
+        "--arch-name",
+        metavar="ARCHNAME",
+        action="append",
+        help="""only build for the given architecture (can
+        be repeated)""",
+    )
 
-argparse_add_verbose(parser)
-_ = parser.add_argument(
-    "--cleanup",
-    action="store_true",
-    help="Remove the work folder after finishing building the recipe",
-)
+    _ = parser.add_argument(
+        "packages_names",
+        nargs="*",
+        metavar="PACKAGENAME",
+        help="list of packages to build (default: all packages from the recipe)",
+    )
 
-args = parser.parse_args()
-logging.basicConfig(format=LOGGING_FORMAT, level=args.verbose)
-repo = Repo(paths.RECIPE_DIR, paths.REPO_DIR)
-builder = Builder(paths.WORK_DIR, paths.REPO_DIR)
-arch_packages: Optional[Dict[str, Optional[List[Package]]]] = None
+    argparse_add_verbose(parser)
+    argparse_add_cleanup(parser)
+    args = parser.parse_args()
+    logging.basicConfig(format=LOGGING_FORMAT, level=args.verbose)
+    builder = Builder(paths.WORK_DIR, paths.REPO_DIR)
 
-with Builder(os.path.join(paths.WORK_DIR, args.recipe_name), paths.REPO_DIR) as builder:
-    recipe_bundle = parse_recipe(f"package/{args.recipe_name}")
-    build_matrix: Optional[Dict[str, Optional[List[Package]]]] = None
-    if args.arch_name or args.packages_names:
-        build_matrix = {}
-        for arch, recipes in recipe_bundle.items():
-            if args.package_name:
-                build_matrix[arch] = [
-                    recipes.packages[pkg_name] for pkg_name in args.package_name
-                ]
-            else:
-                build_matrix[arch] = None
+    with Builder(
+        os.path.join(paths.WORK_DIR, args.recipe_name), paths.REPO_DIR
+    ) as builder:
+        recipe_bundle = parse_recipe(f"package/{args.recipe_name}")
+        build_matrix: Optional[Dict[str, Optional[List[Package]]]] = None
+        if args.arch_name or args.packages_names:
+            build_matrix = {}
+            for arch, recipes in recipe_bundle.items():
+                if args.package_name:
+                    build_matrix[arch] = [
+                        recipes.packages[pkg_name]
+                        for pkg_name in args.package_name
+                    ]
+                else:
+                    build_matrix[arch] = None
 
-    rmtree(builder.work_dir, ignore_errors=True)
-    try:
-        success: bool = builder.make(recipe_bundle, build_matrix, False)
-
-    finally:
-        usage = disk_usage(paths.WORK_DIR)
-        logger.info(
-            "work_dir: %s/%s",
-            sizeof_fmt(usage.used),
-            sizeof_fmt(usage.total),
-        )
-        usage = disk_usage(paths.REPO_DIR)
-        logger.info(
-            "repo_dir: %s/%s",
-            sizeof_fmt(usage.used),
-            sizeof_fmt(usage.total),
-        )
-
-    if args.cleanup:
         rmtree(builder.work_dir, ignore_errors=True)
-        for recipe in recipe_bundle:
-            builder.docker.images.remove(builder.IMAGE_PREFIX + recipe.image)
+        try:
+            success: bool = builder.make(recipe_bundle, build_matrix, False)
 
-    if not success:
-        sys.exit(1)
+        finally:
+            print_disk_usage()
 
-    make_index(paths.REPO_DIR)
+        if args.cleanup:
+            cleanup(builder, recipe_bundle)
+
+        if not success:
+            sys.exit(1)
+
+        make_index(paths.REPO_DIR)
+
+
+if __name__ == "__main__":
+    main()
--- scripts/repo_build.py
+++ scripts/repo_build.py
@@ -14,11 +14,13 @@ from typing import (
 from shutil import disk_usage, rmtree
 from build import paths
 from build.repo import Repo, PackageStatus
-from toltec.recipe import Package  # type: ignore
+from toltec.recipe import Package, RecipeBundle  # type: ignore
 from toltec import parse_recipe  # type: ignore
 from toltec.builder import Builder  # type: ignore
 from toltec.repo import make_index  # type: ignore
 from toltec.util import argparse_add_verbose, LOGGING_FORMAT  # type: ignore
+
+logger = logging.getLogger(__name__)
 
 
 def sizeof_fmt(num: float, suffix: str = "B") -> str:
@@ -31,113 +33,138 @@ def sizeof_fmt(num: float, suffix: str = "B") -> str:
     return f"{num:.1f}Yi{suffix}"
 
 
-logger = logging.getLogger(__name__)
+def cleanup(builder: Builder, recipe_bundle: RecipeBundle) -> None:
+    """Perform cleanup to reduce space usage"""
+    rmtree(builder.work_dir, ignore_errors=True)
+    for recipe in recipe_bundle.values():
+        builder.docker.images.remove(builder.IMAGE_PREFIX + recipe.image)
 
-parser = argparse.ArgumentParser(description=__doc__)
 
-_ = parser.add_argument(
-    "-d",
-    "--diff",
-    action="store_true",
-    help="only keep new packages that do not exist on the remote repository",
-)
+def print_disk_usage() -> None:
+    """Print the current disk usage for important paths"""
+    usage = disk_usage(paths.WORK_DIR)
+    logger.info(
+        "work_dir: %s/%s",
+        sizeof_fmt(usage.used),
+        sizeof_fmt(usage.total),
+    )
+    usage = disk_usage(paths.REPO_DIR)
+    logger.info(
+        "repo_dir: %s/%s",
+        sizeof_fmt(usage.used),
+        sizeof_fmt(usage.total),
+    )
 
-argparse_add_verbose(parser)
-_ = parser.add_argument(
-    "--cleanup",
-    action="store_true",
-    help="Remove the work folder after finishing building the recipe",
-)
 
-group = parser.add_mutually_exclusive_group()
+def argparse_add_cleanup(parser: argparse.ArgumentParser) -> None:
+    """Add the cleanup argument"""
+    _ = parser.add_argument(
+        "--cleanup",
+        action="store_true",
+        help="Remove the work folder after finishing building the recipe",
+    )
 
-_ = group.add_argument(
-    "-l",
-    "--local",
-    action="store_true",
-    help="""by default, packages missing from the local repository are not
-    rebuilt if they already exist on the remote repository — pass this flag to
-    disable this behavior""",
-)
 
-_ = group.add_argument(
-    "-r",
-    "--remote-repo",
-    default="https://toltec-dev.org/testing",
-    metavar="URL",
-    help="""root of a remote repository used to know which packages
-    are already built (default: %(default)s)""",
-)
+def main() -> None:  # pylint: disable=R0914
+    """Build the repo"""
 
-args = parser.parse_args()
-remote = args.remote_repo if not args.local else None
-logging.basicConfig(format=LOGGING_FORMAT, level=args.verbose)
+    parser = argparse.ArgumentParser(description=__doc__)
 
-repo = Repo(paths.RECIPE_DIR, paths.REPO_DIR)
-results = repo.fetch_packages(remote)
+    _ = parser.add_argument(
+        "-d",
+        "--diff",
+        action="store_true",
+        help="only keep new packages that do not exist on the remote repository",
+    )
 
-os.makedirs(paths.REPO_DIR, exist_ok=True)
-make_index(paths.REPO_DIR)
+    argparse_add_verbose(parser)
+    argparse_add_cleanup(parser)
 
-fetched = results[PackageStatus.Fetched]
-missing = results[PackageStatus.Missing]
-ordered_missing = repo.order_dependencies(
-    [repo.generic_recipes[name] for name in missing]
-)
+    group = parser.add_mutually_exclusive_group()
 
-for generic_recipe in ordered_missing:
-    # Will need to rework toltec_old.repo into something inline and actually easy to work
-    # with Currently generic_recipe is a Dict[str, Recipe] where the index is the arch. Every
-    # single entry will have the same path, so we can use that for the name of the generic
-    # recipe we are actually building.
-    name = os.path.basename(next(iter(generic_recipe.values())).path)
-    if missing[name]:
-        with Builder(os.path.join(paths.WORK_DIR, name), paths.REPO_DIR) as builder:
-            rmtree(builder.work_dir, ignore_errors=True)
-            recipe_bundle = parse_recipe(os.path.join(paths.RECIPE_DIR, name))
-            build_matrix: Optional[Dict[str, Optional[List[Package]]]] = None
-            old_build_matrix = missing[name]
-            if old_build_matrix:
-                build_matrix = {}
+    _ = group.add_argument(
+        "-l",
+        "--local",
+        action="store_true",
+        help="""by default, packages missing from the local repository are not
+        rebuilt if they already exist on the remote repository — pass this flag to
+        disable this behavior""",
+    )
 
-                for arch, recipes in old_build_matrix.items():
-                    build_matrix[arch] = [
-                        recipe_bundle[arch].packages[pkg_name]
-                        for pkg_name in recipe_bundle[arch].packages
-                    ]
-            logger.info("Building %s", name)
-            try:
-                builder.make(recipe_bundle, build_matrix, False)
+    _ = group.add_argument(
+        "-r",
+        "--remote-repo",
+        default="https://toltec-dev.org/testing",
+        metavar="URL",
+        help="""root of a remote repository used to know which packages
+        are already built (default: %(default)s)""",
+    )
+    args = parser.parse_args()
+    remote = args.remote_repo if not args.local else None
+    logging.basicConfig(format=LOGGING_FORMAT, level=args.verbose)
 
-            finally:
-                usage = disk_usage(paths.WORK_DIR)
-                logger.info(
-                    "work_dir: %s/%s",
-                    sizeof_fmt(usage.used),
-                    sizeof_fmt(usage.total),
-                )
-                usage = disk_usage(paths.REPO_DIR)
-                logger.info(
-                    "repo_dir: %s/%s",
-                    sizeof_fmt(usage.used),
-                    sizeof_fmt(usage.total),
-                )
+    repo = Repo(paths.RECIPE_DIR, paths.REPO_DIR)
+    results = repo.fetch_packages(remote)
 
-            if args.cleanup:
+    os.makedirs(paths.REPO_DIR, exist_ok=True)
+    make_index(paths.REPO_DIR)
+
+    fetched = results[PackageStatus.Fetched]
+    missing = results[PackageStatus.Missing]
+    ordered_missing = repo.order_dependencies(
+        [repo.generic_recipes[name] for name in missing]
+    )
+
+    for generic_recipe in ordered_missing:
+        # Will need to rework toltec_old.repo into something inline and actually easy to work
+        # with Currently generic_recipe is a Dict[str, Recipe] where the index is the arch. Every
+        # single entry will have the same path, so we can use that for the name of the generic
+        # recipe we are actually building.
+        name = os.path.basename(next(iter(generic_recipe.values())).path)
+        if missing[name]:
+            with Builder(
+                os.path.join(paths.WORK_DIR, name), paths.REPO_DIR
+            ) as builder:
                 rmtree(builder.work_dir, ignore_errors=True)
-                for recipe in recipe_bundle:
-                    builder.docker.images.remove(builder.IMAGE_PREFIX + recipe.image)
+                recipe_bundle = parse_recipe(
+                    os.path.join(paths.RECIPE_DIR, name)
+                )
+                build_matrix: Optional[Dict[str, Optional[List[Package]]]] = (
+                    None
+                )
+                old_build_matrix = missing[name]
+                if old_build_matrix:
+                    build_matrix = {}
 
-        make_index(paths.REPO_DIR)
+                    for arch in old_build_matrix.keys():
+                        build_matrix[arch] = [
+                            recipe_bundle[arch].packages[pkg_name]
+                            for pkg_name in recipe_bundle[arch].packages
+                        ]
+                logger.info("Building %s", name)
+                try:
+                    builder.make(recipe_bundle, build_matrix, False)
 
-if args.diff:
-    for name in fetched:
-        for packages in fetched[name].values():
-            for package in packages:
-                filename = package.filename()
-                local_path = os.path.join(repo.repo_dir, filename)
-                os.remove(local_path)
+                finally:
+                    print_disk_usage()
 
-make_index(paths.REPO_DIR)
-repo.make_listing()
-repo.make_compatibility()
+                if args.cleanup:
+                    cleanup(builder, recipe_bundle)
+
+            make_index(paths.REPO_DIR)
+
+    if args.diff:
+        for name in fetched:
+            for packages in fetched[name].values():
+                for package in packages:
+                    filename = package.filename()
+                    local_path = os.path.join(repo.repo_dir, filename)
+                    os.remove(local_path)
+
+    make_index(paths.REPO_DIR)
+    repo.make_listing()
+    repo.make_compatibility()
+
+
+if __name__ == "__main__":
+    main()
